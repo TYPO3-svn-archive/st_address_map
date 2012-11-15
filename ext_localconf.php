a1ba65e5141fda2f0ad2c0a89fc226c293d64926
scheibo <scheibo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-11-15 14:00:49 +0000

    set Version 0.2.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st_address_map/tags/0.2.2@67864 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,6 @@ if (!defined ('TYPO3_MODE')) {
  	die ('Access denied.');
 }
 
-## WOP:[pi][1][addType]
+include_once(t3lib_extMgm::extPath('st_address_map') . 'lib/user_st_addressmapOnCurrentPage.php'); // Conditions for JS including
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_staddressmap_pi1.php', '_pi1', 'list_type', 1);
 ?>
