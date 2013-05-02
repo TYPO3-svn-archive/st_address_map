da610f5b3a16f64cad22da2b0b2795446f43432a
scheibo <scheibo@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-02 22:39:30 +0000

    set version 0.3.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/st_address_map/trunk@75070 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "st_address_map".
  *
- * Auto generated 18-02-2013 08:24
+ * Auto generated 03-05-2013 00:37
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'With the extension st_address_map you are able to show addresses out of tt_address in Google Maps. Further it is possible to show the addresses in a list.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.3.1',
+	'version' => '0.3.2',
 	'dependencies' => 'tt_address',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-4.7.99',
+			'typo3' => '4.5.0-6.1.99',
 			'tt_address' => '2.2.0-2.3.3',
 		),
 		'conflicts' => array(
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"1704";s:12:"ext_icon.gif";s:4:"8cae";s:17:"ext_localconf.php";s:4:"8a94";s:14:"ext_tables.php";s:4:"268b";s:14:"ext_tables.sql";s:4:"dda4";s:12:"flexform.xml";s:4:"c06d";s:16:"locallang_db.xml";s:4:"a236";s:17:"locallang_tca.php";s:4:"0cc0";s:10:"README.txt";s:4:"96a6";s:14:"doc/manual.sxw";s:4:"3a8d";s:19:"doc/wizard_form.dat";s:4:"e118";s:20:"doc/wizard_form.html";s:4:"437e";s:39:"lib/user_st_addressmapOnCurrentPage.php";s:4:"8f73";s:33:"pi1/class.tx_staddressmap_pi1.php";s:4:"e44b";s:17:"pi1/locallang.xml";s:4:"c742";s:16:"static/style.css";s:4:"4b42";s:20:"static/template.html";s:4:"d9fc";s:23:"static/tx_addressmap.js";s:4:"ec6a";s:46:"static/selectbox/jquery.selectbox-0.1.3.min.js";s:4:"c76e";s:37:"static/selectbox/jquery.selectbox.css";s:4:"8cb8";s:33:"static/selectbox/select-icons.png";s:4:"4490";s:35:"static/st_address_map/constants.txt";s:4:"65b8";s:31:"static/st_address_map/setup.txt";s:4:"5afc";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"1704";s:12:"ext_icon.gif";s:4:"8cae";s:17:"ext_localconf.php";s:4:"8a94";s:14:"ext_tables.php";s:4:"268b";s:14:"ext_tables.sql";s:4:"dda4";s:12:"flexform.xml";s:4:"c06d";s:16:"locallang_db.xml";s:4:"a236";s:17:"locallang_tca.php";s:4:"0cc0";s:10:"README.txt";s:4:"96a6";s:14:"doc/manual.sxw";s:4:"3a8d";s:19:"doc/wizard_form.dat";s:4:"c7ce";s:20:"doc/wizard_form.html";s:4:"6039";s:39:"lib/user_st_addressmapOnCurrentPage.php";s:4:"8f73";s:33:"pi1/class.tx_staddressmap_pi1.php";s:4:"f4c2";s:17:"pi1/locallang.xml";s:4:"c742";s:16:"static/style.css";s:4:"4b42";s:20:"static/template.html";s:4:"d9fc";s:23:"static/tx_addressmap.js";s:4:"ec6a";s:46:"static/selectbox/jquery.selectbox-0.1.3.min.js";s:4:"c76e";s:37:"static/selectbox/jquery.selectbox.css";s:4:"8cb8";s:33:"static/selectbox/select-icons.png";s:4:"4490";s:35:"static/st_address_map/constants.txt";s:4:"65b8";s:31:"static/st_address_map/setup.txt";s:4:"e073";}',
 	'suggests' => array(
 	),
 );
